d183e411b945e19f674cc4bae9fa4a66c5fb9d31
Froggkrl <kir07kot@gmail.com>
2025-12-15 15:01:11 +0300

    commit-1

--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movies struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movies
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
-    
+	r := mux.NewRouter()
+
+	movies = append(movies, Movies{ID: "1", Isbn: "438277", Title: "Iron man", Director: &Director{FirstName: "John", LastName: "Doe"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Server starting at port:8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
